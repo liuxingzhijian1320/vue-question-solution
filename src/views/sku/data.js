915fec29eb05d44e83110bba61f0e567005b7f4f
zhooson <861077046@qq.com>
2023-04-20 15:09:49 +0800

    fix: sku

--- src/views/sku/data.js
+++ src/views/sku/data.js
@@ -10,14 +10,14 @@ const specList = [
         id: 2,
         name: "紫色",
       },
-      {
-        id: 3,
-        name: "白色",
-      },
-      {
-        id: 4,
-        name: "黑色",
-      },
+      // {
+      //   id: 3,
+      //   name: "白色",
+      // },
+      // {
+      //   id: 4,
+      //   name: "黑色",
+      // },
     ],
   },
   {
@@ -31,22 +31,43 @@ const specList = [
         id: 6,
         name: "套餐二",
       },
+      // {
+      //   id: 7,
+      //   name: "套餐三",
+      // },
+      // {
+      //   id: 8,
+      //   name: "套餐四",
+      // },
+    ],
+  },
+  {
+    title: "尺寸",
+    list: [
       {
-        id: 7,
-        name: "套餐三",
+        id: 50,
+        name: "M",
       },
       {
-        id: 8,
-        name: "套餐四",
+        id: 60,
+        name: "S",
       },
+      // {
+      //   id: 7,
+      //   name: "套餐三",
+      // },
+      // {
+      //   id: 8,
+      //   name: "套餐四",
+      // },
     ],
   },
 ];
 
 const skuList = [
-  { id: 1608188117177, stock: 10, specs: ["套餐一", "红色"] },
-  { id: 1608188117178, stock: 0, specs: ["红色", "套餐二"] },
-  { id: 1608188117189, stock: 1, specs: ["紫色", "套餐一"] },
+  { id: 111, stock: 3, price: 100, specs: ["套餐一", "红色", "M"] },
+  { id: 222, stock: 0, price: 200, specs: ["红色", "套餐二", "S"] },
+  { id: 333, stock: 1, price: 300, specs: ["紫色", "套餐一", "M"] },
 ];
 
 export { specList, skuList };
--- src/views/sku/sku copy.vue
+++ src/views/sku/sku copy.vue
@@ -60,11 +60,13 @@
       </div>
     </div>
 
-    <div class="selected" v-if="isShowSpecTitle">
+    <div class="selected">
       ¥： {{ result.price }}
-      <br>
-      已选规格：
-      <div class="select-box" v-for="(item,index) in specList" :key="index">
+    </div>
+
+    <div class="selected">
+      已选规格：<div class="select-box" v-for="(item,index) in specList"
+        :key="index">
         {{ item.title }}：{{ selectSpec[item.title] }}
       </div>
     </div>
@@ -72,6 +74,7 @@
 
 </template>
 <script>
+// 第一步引入数据 这里引入的数据就是上面的 specList  skuList
 import { specList, skuList } from "./data";
 
 export default {
@@ -88,39 +91,39 @@ export default {
   },
 
   created() {
-    // 模拟异步请求数据
-    setTimeout(() => {
-      // 对返回的数据处理
-      this.skuList = skuList;
-      console.log("skuList", this.skuList);
-      // 初始化选择数据的对象
-      specList.forEach((item) => {
-        this.$set(this.selectSpec, item.title, "");
-      });
-      // 将规格数据处理成我们视图所需要的数据类型
-      this.specList = specList.map((item) => {
-        return {
-          title: item.title,
-          list: item.list.map((code) => {
-            return {
-              ...code,
-              able: this.isAble(item.title, code.name),
-            };
-          }),
-        };
-      });
-      console.log("specList", this.specList);
+    //  第二步  处理数据
+    this.skuList = skuList;
+    // 初始化选择数据的对象
+    specList.forEach((item) => {
+      this.$set(this.selectSpec, item.title, "");
+    });
+    // this.$set(this.selectSpec, "数量", this.count);
+    // console.log("selectSpec", { ...this.selectSpec });
 
-      // 默认选中代码
-      // this.selectSpec = {
-      //   颜色: "红色",
-      //   套餐: "套餐一",
-      //   尺寸: "M",
-      // };
-      // this.isAble("颜色", "红色", "M");
-      // this.isShowSpecTitle = true;
-      // this.updatePrice();
-    }, 2000);
+    // 将规格数据处理成我们视图所需要的数据类型
+    this.specList = specList.map((item) => {
+      return {
+        title: item.title,
+        list: item.list.map((code) => {
+          return {
+            ...code,
+            //  判断是否可以选择
+            //  这里相当于init 初始化数据  this.isAble() 核心判断逻辑
+            able: this.isAble(item.title, code.name), // 注释的调试看逻辑代码 false
+          };
+        }),
+      };
+    });
+    console.log("specList", this.specList);
+    // 注释的调试看逻辑代码
+
+    // 初始化默认数据
+    // this.selectSpec = {
+    //   颜色: "红色",
+    //   套餐: "套餐一",
+    //   内存: "64G",
+    // };
+    // this.isAble("颜色", "红色");
   },
   methods: {
     // 判断每个key是否empty
@@ -135,18 +138,20 @@ export default {
     },
 
     updatePrice() {
+      // console.log(19, this.skuList);
+      // console.log(234, this.specList);
+
       let selectSpec = [];
-      // this.selectSpecDetail = [];
+      this.selectSpecDetail = [];
 
       for (let key in this.selectSpec) {
         selectSpec.push(this.selectSpec[key]);
-        // this.selectSpecDetail.push({
-        //   title: key,
-        //   value: this.selectSpec[key],
-        // });
+        this.selectSpecDetail.push({
+          title: key,
+          value: this.selectSpec[key],
+        });
       }
 
-      // [1,2,3] === [2,1,3]
       this.result = this.skuList.find(
         (c) =>
           c.specs.length === selectSpec.length &&
@@ -172,27 +177,46 @@ export default {
       }
     },
 
+    // 核心判断逻辑
+    // 判断规格是否可以被选择  核心函数 key当前的规格的 title value规格值
     isAble(key, value) {
+      // console.log("key, value", key, value);
       // 深拷贝 避免被影响
       var copySelectSpec = JSON.parse(JSON.stringify(this.selectSpec));
+
+      // 用对象的好处就在这了 直接赋值当前验证项
       copySelectSpec[key] = value;
+      // console.log("copySelectSpec", copySelectSpec, key, value);
+      // 用数组的 some 方法 效率高 符合条件直接退出循环
       let flag = this.skuList.some((item) => {
+        // 条件判断 核心逻辑判断
+        // console.log(item)
         var i = 0;
+        // 这个for in 循环的逻辑就对底子不深的人来说就看不懂了
+        // 原理就是循环已经选中的 和 正在当前对比的数据 和 所有的sku对比
+        // 只有当前验证的所有项满足sku中的规格或者其他规格为空时 即满足条件
+        // 稍微有点复杂 把注释的调试代码打开就调试下就可以看懂了
         for (let k in copySelectSpec) {
+          // console.log(copySelectSpec[k])  // 注释的调试看逻辑代码
+          // console.log(123, item.specs.includes(copySelectSpec[k]));
           if (
             copySelectSpec[k] != "" &&
             item.specs.includes(copySelectSpec[k])
+            // item.specs.findIndex((c) => c.name === copySelectSpec[k]) > -1
           ) {
+            // console.log(item)
             i++;
           } else if (copySelectSpec[k] == "") {
             i++;
           }
         }
+        // 符合下面条件就退出了 不符合会一直循环知道循环结束没有符合的条件就 return false 了
+        // console.log(i) // 注释的调试看逻辑代码
         return i == specList.length;
       });
+      // console.log(flag);
       return flag;
     },
-
     // 点击事件
     changeSpec(key, value, able) {
       if (!able) return;
@@ -202,9 +226,11 @@ export default {
       } else {
         this.selectSpec[key] = value;
       }
+      // forEach循环改变原数组
       this.specList.forEach((item) => {
         item.list.forEach((code) => {
           code.able = this.isAble(item.title, code.name);
+          // console.log(code.name, code.able);
         });
       });
 
